c257a6129875321bcd1e235336022e8c59948b79
rookshanks <54780738+rookshanks@users.noreply.github.com>
2020-01-04 19:56:21 -0700

    first
    just need to add something to scale to device

---
+++ script.js
@@ -0,0 +1,29 @@
+function my_timer(time, redirect) {
+    if (time == 0) {
+        window.location.replace(redirect)
+    } else {
+        time = time - 1;
+        document.getElementById("time").textContent=time;
+        setTimeout(function(){my_timer(time, redirect)}, 1000);
+    }
+}
+
+function getUrlVars() {
+    let parts = window.location.href;
+    let interval = [parts.indexOf("time="), parts.indexOf("&")]
+    let end = parts.indexOf("redirect=");
+    let time = parts.slice(interval[0] + 5, interval[1])
+    let myurl = parts.slice(end + 9);
+    console.log(typeof myurl)
+    console.log(time)
+    console.log(myurl);
+    return [myurl, time]
+}
+
+
+let deets = getUrlVars();
+let originalurl = deets[0]
+let time = parseInt(deets[1], 10)
+console.log(originalurl);
+my_timer(time, originalurl);
+
